fe08185795e06f33634525997b9ed9b7cb88d33d
Andrej Podhradsky <apodhrad@redhat.com>
2019-01-16 22:56:35 +0100

    Use commas
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- get_funds.sh
+++ get_funds.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 get_price() {
-  PRICE=$(curl -s https://www.ing.cz/ing-podilove-fondy/kompletni-nabidka-ing-podilovych-fondu/$1/ | grep $2 | head -1 | awk '{$1=$1};1'| awk -F' ' '{ print $1 }')
-  echo "$1;$PRICE;$2"
+  PRICE=$(curl -s https://www.ing.cz/ing-podilove-fondy/kompletni-nabidka-ing-podilovych-fondu/$1/ | grep $2 | head -1 | awk '{$1=$1};1'| awk -F' ' '{ print $1 }' | tr ',' '.')
+  echo "$1,$PRICE,$2"
 }
 
 get_price_ing() {
-  PRICE=$(curl -s https://www.ing.cz/ing-podilove-fondy/nabidka-fondu/zakladni-nabidka-ing-podilovych-fondu-bez-poplatku/$1/ | grep $2 | grep -v '=' | head -1 | awk '{$1=$1};1' | awk -F' ' '{ print $1 }')
-  echo "$1;$PRICE;$2"
+  PRICE=$(curl -s https://www.ing.cz/ing-podilove-fondy/nabidka-fondu/zakladni-nabidka-ing-podilovych-fondu-bez-poplatku/$1/ | grep $2 | grep -v '=' | head -1 | awk '{$1=$1};1' | awk -F' ' '{ print $1 }' | tr ',' '.')
+  echo "$1,$PRICE,$2"
 }
 
-echo "id;price;currency"
+echo "id,price,currency"
 get_price "blackrock-world-healthscience" "USD"
 get_price "franklin-technology" "USD"
 get_price "fidelity-global-dividend" "CZK"
