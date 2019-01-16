f6c352a70ca61df0f657c4002d6f5bf727a5aad7
Andrej Podhradsky <apodhrad@redhat.com>
2019-01-16 22:27:41 +0100

    Add a script for getting prices of some ing funds
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

---
+++ get_funds.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+get_price() {
+  PRICE=$(curl -s https://www.ing.cz/ing-podilove-fondy/kompletni-nabidka-ing-podilovych-fondu/$1/ | grep $2 | head -1 | awk '{$1=$1};1'| awk -F' ' '{ print $1 }')
+  echo "$1;$PRICE;$2"
+}
+
+get_price_ing() {
+  PRICE=$(curl -s https://www.ing.cz/ing-podilove-fondy/nabidka-fondu/zakladni-nabidka-ing-podilovych-fondu-bez-poplatku/$1/ | grep $2 | grep -v '=' | head -1 | awk '{$1=$1};1' | awk -F' ' '{ print $1 }')
+  echo "$1;$PRICE;$2"
+}
+
+get_price "blackrock-world-healthscience" "USD"
+get_price "franklin-technology" "USD"
+get_price "fidelity-global-dividend" "CZK"
+get_price "fidelity-asian-special-situations" "CZK"
+get_price_ing "ing-aria-lion-aggresive" "CZK"
+get_price_ing "ing-aria-lion-dynamic" "CZK"
+get_price_ing "ing-aria-lion-balanced" "CZK"
+get_price_ing "ing-aria-lion-moderate" "CZK"
